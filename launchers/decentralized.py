351dbf7e3d29340977654168e4994f3391b70ee2
AmitTsvi <amit.tsvi@gmail.com>
2021-02-28 17:45:28 +0200

    debug

--- launchers/decentralized.py
+++ launchers/decentralized.py
@@ -104,7 +104,9 @@ class DecentralizedLauncher(BaseLauncher):
         unique_agents = []
         subsocieties = []
         unique_subsocieties = []
-
+        print("len(args.parents)="+str(len(args.parents)))
+        print("num_primitives="+str(num_primitives))
+        print("redundancy="+str(redundancy))
         replay_buffer = lambda: PathMemory(
             max_replay_buffer_size=args.max_buffer_size*redundancy)
         for k in range(len(args.parents)):
@@ -185,7 +187,7 @@ class DecentralizedLauncher(BaseLauncher):
             state_dim=task_progression.state_dim,
             action_dim=task_progression.action_dim,
             args=args)
-
+        print("arg.clone="+args.clone)
         if args.clone:
             agents, unique_agents, subsocieties, unique_subsocieties = cls.create_cloned_agents(
                 agent_builder=agent_builder,
