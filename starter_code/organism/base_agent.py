0d423a9b5456daf941e32e7a82558eac718bde21
AmitTsvi <amit.tsvi@gmail.com>
2021-03-21 12:15:32 +0200

    calculating weight change in load 1

--- starter_code/organism/base_agent.py
+++ starter_code/organism/base_agent.py
@@ -57,7 +57,6 @@ class BaseAgent(nn.Module):
             old_model = copy.deepcopy(self.networks[name])
             self.networks[name].load_state_dict(agent_state_dict[name])
             for p1, p2 in zip(old_model.parameters(), self.networks[name].parameters()):
-                print(p1.data)
                 changed += torch.isclose(p1.data, p2.data, rtol=10**-3).sum().item()
         if not reset_optimizer:
             for name in self.optimizers:
